2f4304cf239be1952c3f221b9e4eb0cb1db3522c
Kauã <kauaalvesworkplace@gmail.com>
2025-01-23 15:31:07 -0300

    project v2

--- index.js
+++ index.js
@@ -1,53 +1,100 @@
 const express = require("express");
 const { exec } = require("child_process");
-const cors = require('cors');
-const path = require('path');
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
+let collectionAgents = [];
+
+// ----------- Middlewares -----------
 
 app.use(express.json());
 
 // APENAS PARA DESENVOLVIMENTO
 app.use(cors());
 
+// Servindo arquivos estáticos
+app.use("/static", express.static(path.join(__dirname, "public", "static")));
+app.use("/models", express.static(path.join(__dirname, "public", "models")));
 
-app.use('/static', express.static(path.join(__dirname, 'public', 'static')));
-app.use('/models', express.static(path.join(__dirname, 'public', 'models')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// ----------- Routes -----------
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/agents", (req, res) => {
+    const validAgents = collectionAgents.filter(agent => agent !== undefined);
+
+    res.status(200).json(validAgents);
+});
+app.post("/changePosition", (req, res) => {
+    const agents = req.body;  // Aqui o req.body é o array completo de agentes
+
+    if (!agents || !Array.isArray(agents)) {
+        return res.status(400).json({ error: "Faltando ou formato inválido para o array de agentes" });
+    }
+
+    agents.forEach(agent => {
+      const { id, name, x, y, z } = agent;
+
+      if (id == null || !name || x == null || y == null) {
+        return res.status(400).json({ error: "Faltando parâmetros na requisição de algum agente" });
+      }
+
+      if (!collectionAgents[id]) {
+        collectionAgents[id] = { id, name, x, y };
+      } else {
+        collectionAgents[id].name = name;
+        collectionAgents[id].x = x;
+        collectionAgents[id].y = y;
+        collectionAgents[id].z = z;
+      }
+    });
+
+    console.log("Coleção atualizada:", collectionAgents);
+
+    res.status(200).json({
+        message: "Posição dos agentes atualizada com sucesso",
+        agents: collectionAgents.filter(Boolean), // Remove `undefined` do array
+    });
+});
+
+
 app.post("/createPort", (req, res) => {
-    const { rate, path } = req.body;
-    
-    if (!path || !rate) {
-        return res.status(400).json({ error: "Faltando parâmetros path ou rate" });
-    }
+  const { rate, path } = req.body;
 
-    // Construindo o comando para rodar o script `serial-service.js`
-    const command = `node serial-service.js -p ${path} -r ${rate}`;
+  if (!path || !rate) {
+    return res.status(400).json({ error: "Faltando parâmetros path ou rate" });
+  }
 
-    try {
-        // Executando o comando
-        exec(command, (err, stdout, stderr) => {
-            if (err) {
-                console.error("Erro no comando:", err);
-                return res.status(500).json({ error: "Erro ao executar o comando, Verifique a versão do seu node." });
-            }
+  // Construindo o comando para rodar o script `serial-service.js`
+  const command = `node serial-service.js -p ${path} -r ${rate}`;
 
-            // Logando o stdout e stderr
-            if (stdout) console.log(`stdout: ${stdout}`);
-            if (stderr) console.log(`stderr: ${stderr}`);
+  try {
+    // Executando o comando
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.error("Erro no comando:", err);
+        return res
+          .status(500)
+          .json({ error: "Erro ao executar o comando, Verifique a versão do seu node." });
+      }
 
-            res.status(200).json({ message: "Porta serial criada com sucesso!" });
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro interno do servidor." });
-    }
+      // Logando o stdout e stderr
+      if (stdout) console.log(`stdout: ${stdout}`);
+      if (stderr) console.log(`stderr: ${stderr}`);
+
+      res.status(200).json({ message: "Porta serial criada com sucesso!" });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro interno do servidor." });
+  }
 });
 
 app.listen(3000, () => {
-    console.log("Servidor Express rodando na porta 3000");
+  console.log("Servidor Express rodando na porta 3000");
 });
